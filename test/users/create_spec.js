3f95c85044a4b7386ed6238534f1f3cba92ee147
James Darby <jamesdarby1@hotmail.com>
2020-02-07 13:06:33 +0000

    updated test

--- test/users/create_spec.js
+++ test/users/create_spec.js
@@ -1,13 +1,15 @@
-const User = require(../../models/user)
-const jst = require('jsonwebtoken')
-const { secret } = require('../../config/environment')
+// const User = require(../../models/user)
+// const jst = require('jsonwebtoken')
+// const { secret } = require('../../config/environment')
 
-const testUserData = {
-  username: 'test',
-  email: 'test@email',
-  image: 'image.jpg',
-  skills: 'Asian',
-  location: 'London',
-  password: 'test',
-  passwordConfirmation: 'test'
-}
+// const testUserData = {
+//   username: 'test',
+//   email: 'test@email',
+//   image: 'image.jpg',
+//   skills: 'Asian',
+//   location: 'London',
+//   password: 'test',
+//   passwordConfirmation: 'test'
+// }
+
+// describe('POST /users', () => {
