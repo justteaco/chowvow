010b063cce3d533dff9074554c35143d7d8ada2f
Tbanks9 <timbanks9@hotmail.com>
2020-02-07 12:33:47 +0000

    added users.js

---
+++ models/user.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose') // needed to create a new schema and model
+// const bcrypt = require('bcrypt') // our library used to hash our users passwords
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true }
+// }, {
+//   timestamps: true
+// })
+
+// module.exports = mongoose.model('User', userSchema)
