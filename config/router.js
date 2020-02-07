52c470f8439fcac1be6f1c542d0f42f4bed9aecb
James Darby <jamesdarby1@hotmail.com>
2020-02-07 13:47:27 +0000

    fixing links in router

--- config/router.js
+++ config/router.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
-const users = require('../controllers/..........')
+const users = require('../controllers/users')
+const auth = require('../controllers/auth')
 //need to bring in secure route
 
 router.route('/chefs')
   .get(users.index)
 
 router.route('/register')
-  .post(users.register)
+  .post(auth.register)
 
 router.route('/login') 
-  .post(users.login)
+  .post(auth.login)
 
 router.route('/profile')
   .get(users.profile)
