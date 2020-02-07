5bb4b3fe861edfe50e0be9fc48b851a814f0d693
EmilySummers <emilysummers36@gmail.com>
2020-02-07 14:05:35 +0000

    added users controller

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const User = require('../models/user')
+
+// create index function that gets all cooks
+function index(req, res) {
+  User
+    .find()
+    // .populate('user')
+    .then(foundUsers => res.status(200).json(foundUsers))
+    .catch(err => res.json(err))
+}
+
+// create show function that gets a cook by their id and returns their profile page 
+function show(req, res) {
+  User
+    .findById(req.params.id)
+    // .populate('user')
+    .then(selectedUser => res.status(200).json(selectedUser))
+    .catch(err => res.json(err))
+}
+
+module.exports = { index, show }
+
+
+// create an update function that finds a cook by their id, updates it with the request body and re-saves (edit profile)
+// create a remove function that finds a cook by their id and deletes it (delete profile)
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+// build express server
 const app = express()
 const { port, dbURI } = require('./config/environment')
 const logger = require('./lib/logger')
+const router = require('./config/router')
 
+// connect mongoose to database (dbURI)
 mongoose.connect(dbURI, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, (err) => {
   if (err) return console.log(err)
   console.log('Mongo is connected')
 })
 
+// set up bodyParser middleware
 app.use(bodyParser.json())
 
+// set up logger middleware
 app.use(logger)
 
+// set up router middleware
+app.use('/api', router)
+
+// get express running on port
 app.listen(port, () => console.log(`Express is listening on port ${port}`))
