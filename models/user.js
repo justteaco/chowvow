604c0c5ed44638a2e0ed22da2e417405697ef172
EmilySummers <emilysummers36@gmail.com>
2020-02-08 15:42:27 +0000

    added seeds

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose') // This is needed to create a new schema an
 const bcrypt = require('bcrypt') // Our chosen our library used to hash passwords
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   image: { type: String, required: true },
   skills: { type: String, required: true },
-  location: { type: String, required: true },
+  city: { type: String, required: true },
+  postcode: { type: String, required: true },
   password: { type: String, required: true }
 }, {
   timestamps: true
