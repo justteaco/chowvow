af0caa6ec3e541382342a706b17a5f3518f202dc
EmilySummers <emilysummers36@gmail.com>
2020-02-08 19:08:15 +0000

    added skills boxes on homepage

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,11 +1,116 @@
 import React from 'react'
 
 const Home = () => (
-  <div className="hero-body">
-    <div className="header">
-      <h1>Chow Vow</h1>
+  <>
+    <div className="hero-body">
+      <div className="header">
+        <h1>Chow Vow</h1>
+      </div>
     </div>
-  </div>
+    <a href="#skills" className="arrow"><img src="https://lh3.googleusercontent.com/proxy/LMBMqvWzvoEvmr8KHteF3BL5hzt1ITCaAOfBAbD-tyHZAPiAdw4nfHBJ1aR1ygrBKAo3vrdHkYMXaCqT2ohbKsh4V9L_7rHaotWGYplDYMxGfuoCDKITMQQzng" /></a>
+    <section>
+      <div className="wrapper" id="skills">
+        <div className="columns is-mobile is-multiline">
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/african2.jpeg" alt="african" />
+            <div className="border">
+              <h2>African</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/caribbean2.jpg" alt="caribbean" />
+            <div className="border">
+              <h2>Caribbean</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/chinese2.jpeg" alt="chinese" />
+            <div className="border">
+              <h2>Chinese</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/french2.jpeg" alt="french" />
+            <div className="border">
+              <h2>French</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/greek.jpeg" alt="greek" />
+            <div className="border">
+              <h2>Greek</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/indian2.jpeg" alt="indian" />
+            <div className="border">
+              <h2>Indian</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/italian2.jpeg" alt="italian" />
+            <div className="border">
+              <h2>Italian</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/japan3.jpeg" alt="japanese" />
+            <div className="border">
+              <h2>Japanese</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/korean5.jpeg" alt="korean" />
+            <div className="border">
+              <h2>Korean</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/mexican3.jpg" alt="mexican" />
+            <div className="border">
+              <h2>Mexican</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/moroccan.jpeg" alt="moroccan" />
+            <div className="border">
+              <h2>Moroccan</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/SE asian2.jpeg" alt="south-east asian" />
+            <div className="border">
+              <h2>South-East Asian</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/spanish3.jpg" alt="spanish" />
+            <div className="border">
+              <h2>Spanish</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/turkish4.jpg" alt="turkish middle-east" />
+            <div className="border">
+              <h2>Turkish/Middle-Eastern</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/vegan2.jpeg" alt="vegan" />
+            <div className="border">
+              <h2>Vegan</h2>
+            </div>
+          </div>
+          <div className="skill column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+            <img src="../../assets/vegan3.jpeg" alt="vegetarian" />
+            <div className="border">
+              <h2>Vegetarian</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
 )
 
 export default Home
