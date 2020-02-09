530815252e84930c58ca69886271850dcd7238ac
Tahirah Jarrett <tahirahjarrett@live.com>
2020-02-09 15:05:46 +0000

    created the index page and the login page for our user and added it to the appjs file as well

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ import 'bulma'
 import Home from './components/common/Home'
 import Navbar from './components/common/Navbar'
 import UserShow from './components/users/UserShow'
+import UserIndex from './components/users/UserIndex'
+
+
+import Login from './components/auth/Login'
+//import SecureRoute from '../lib/secureRoute'
+import ErrorPage from './components/common/ErrorPage'
+
 
 const App = () => (
   <BrowserRouter>
@@ -15,6 +22,9 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/users/:id" component={UserShow} />
+        <Route path="/users" component={UserIndex} />
+        <Route path="/users/login" component={Login} />
+        <Route path="/*" component={ErrorPage} />
       </Switch>
     </>
   </BrowserRouter>
