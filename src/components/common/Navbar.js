e24712b3cc51ab1e253aec8718033a12472f08ed
EmilySummers <emilysummers36@gmail.com>
2020-02-10 11:01:58 +0000

    added dropdown to navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -8,7 +8,29 @@ class Navbar extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link className="navbar-item" to="/">Chow Vow</Link>
-            <Link className="navbar-item" to="/cooks/:skills">Skills</Link>
+            <nav className="navbar is-dark" role="navigation" aria-label="dropdown navigation">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Skills</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item" href="/chefs">African</a>
+                  <a className="navbar-item" href="/chefs">Caribbean</a>
+                  <a className="navbar-item" href="/chefs">Chinese</a>
+                  <a className="navbar-item" href="/chefs">French</a>
+                  <a className="navbar-item" href="/chefs">Greek</a>
+                  <a className="navbar-item" href="/chefs">Indian</a>
+                  <a className="navbar-item" href="/chefs">Italian</a>
+                  <a className="navbar-item" href="/chefs">Japanese</a>
+                  <a className="navbar-item" href="/chefs">Korean</a>
+                  <a className="navbar-item" href="/chefs">Mexican</a>
+                  <a className="navbar-item" href="/chefs">Moroccan</a>
+                  <a className="navbar-item" href="/chefs">South-East Asian</a>
+                  <a className="navbar-item" href="/chefs">Spanish</a>
+                  <a className="navbar-item" href="/chefs">Turkish/Middle-Eastern</a>
+                  <a className="navbar-item" href="/chefs">Vegan</a>
+                  <a className="navbar-item" href="/chefs">Vegetarian</a>
+                </div>
+              </div>
+            </nav>
             <Link className="navbar-item" to="/register">Sign Up</Link>
             <Link className="navbar-item" to="/login">Login</Link>
           </div>
